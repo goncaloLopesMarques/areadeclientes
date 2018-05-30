7e6df840ed32e023a6c62ee34ed77eb0b2bf5dd8
Unknown <goncalo.lopes.m@gmail.com>
2018-05-30 17:02:28 +0100

    urla acabada

--- resources/views/layout/searchPage.blade.php
+++ resources/views/layout/searchPage.blade.php
@@ -1,34 +1,29 @@
-               @if(session()->has('Success'))
-                  <div class="col-md-12 alert alert-success" data-dismiss="alert" aria-label="close"  style="-webkit-appearance: none;">
-                     {{ session()->get('message') }}
-                  </div>
-                  @endif
-                  @if(session('Error'))
-                  <div class="col-md-12 alert alert-danger" style="-webkit-appearance: none;">
-                     {{session('response')}}
-                  </div>
-                  @endif
-<div class="container">
-    <div class="row">
-    <div class="col-md-6 offset-3">
-        <form class="col-md-12" action="{{ url('/presquisar') }}" method="post" style="idth: 100%;">
-    
-         {{ csrf_field() }}
-    
-      <p class="h4 text-center mb-4">Já é nosso cliente?</p>
-
-     <div class="md-form">
-        <i class="fa fa-envelope prefix grey-text"></i>
-        <input type="email" id="email" class="form-control" name="email" required="">
-        <label for="materialFormSubscriptionEmailEx">O seu Email</label>
-      </div>
-
-    <div class="text-center mt-4">
-        <button class="btn btn-default btn-rounded mb-4 waves-effect waves-light" type="submit">Pesquisar<i class="fa fa-paper-plane-o ml-2"></i></button>
-    </div>
-</form>
-        </div>
-<!-- Material form subscription -->
-
+@if(session()->has('Success'))
+<div class="col-md-12 alert alert-success" data-dismiss="alert" aria-label="close"  style="-webkit-appearance: none;">
+   {{ session()->get('message') }}
 </div>
+@endif
+@if(session('Error'))
+<div class="col-md-12 alert alert-danger" style="-webkit-appearance: none;">
+   {{session('response')}}
+</div>
+@endif
+<div class="container">
+   <div class="row">
+      <div class="col-md-6 offset-3">
+         <form class="col-md-12" action="{{ url('/pesquisar/pesquisar') }}" method="post" style="idth: 100%;">
+            {{ csrf_field() }}
+            <p class="h4 text-center mb-4">Já é nosso cliente?</p>
+            <div class="md-form">
+               <i class="fa fa-envelope prefix grey-text"></i>
+               <input type="email" id="email" class="form-control" name="email" required="">
+               <label for="materialFormSubscriptionEmailEx">O seu Email</label>
+            </div>
+            <div class="text-center mt-4">
+               <button class="btn btn-default btn-rounded mb-4 waves-effect waves-light" type="submit">Pesquisar<i class="fa fa-paper-plane-o ml-2"></i></button>
+            </div>
+         </form>
+      </div>
+      <!-- Material form subscription -->
+   </div>
 </div>
